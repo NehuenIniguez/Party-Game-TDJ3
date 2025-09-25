460efe5e570adf30b4de04cb7e638ada15e68a60
NehuenIniguez <164099712+NehuenIniguez@users.noreply.github.com>
2025-09-25 15:00:13 -0300

    implementacion sprites

--- Assets/Scripts/PasoNivel.cs
+++ Assets/Scripts/PasoNivel.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,16 +6,23 @@ public class PasoNivel : MonoBehaviour
 {
 
 
-    void OnTriggerEnter2D(Collider2D collision)
+  void OnTriggerEnter2D(Collider2D collision)
+  {
+    if (collision.CompareTag("Player"))
     {
-        if (collision.CompareTag("Player"))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
+      StartCoroutine(PasoNivelCoroutine());
     }
-  
+  }
+
   public void pasonivel()
   {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  }
+
+  IEnumerator PasoNivelCoroutine()
+  { 
+    gameObject.GetComponent<Animator>().SetTrigger("Recolectado");
+    yield return new WaitForSeconds(1f);
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
   }
 }
